8fcdb8341aaebf641184b1492cbaf35f868bedf4
Thushara Thiwanka <46595464+thusharathiwanka@users.noreply.github.com>
2022-05-06 11:55:57 +0530

    CREATE: payment routes added

--- payment-service/routes/payment.route.js
+++ payment-service/routes/payment.route.js
@@ -1,18 +1,9 @@
 const router = require("express").Router();
-const {
-	getFeedbacksTotal,
-	getFeedbacks,
-	getFeedback,
-	deleteFeedback,
-	saveFeedback,
-	getFeedbacksOfUser,
-} = require("../controllers/feedback.controller");
 
-router.get("/", getFeedbacks);
-router.post("/", saveFeedback);
-router.get("/user/:id", getFeedbacksOfUser);
-router.get("/total", getFeedbacksTotal);
-router.get("/:id", getFeedback);
-router.delete("/:id", deleteFeedback);
+const { addPayment, getAllPayment, getPaymentTotal } = require("../controllers/payment.controller");
+
+router.post("/", addPayment);
+router.get("/", getAllPayment);
+router.get("/total", getPaymentTotal);
 
 module.exports = router;
