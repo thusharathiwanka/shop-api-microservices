d8281ce808adf3c17eab0a08db282846a5debcc8
Thushara Thiwanka <46595464+thusharathiwanka@users.noreply.github.com>
2022-05-06 11:17:28 +0530

    UPDATE: package.json updated

--- payment-service/models/payment.model.js
+++ payment-service/models/payment.model.js
@@ -1,10 +1,13 @@
 const mongoose = require("mongoose");
 
-const paymentSchema = mongoose.Schema({
-	name: { type: String, require: true, trim: true },
-	date: { type: String, require: true, trim: true },
-	amount: { type: String, require: true, trim: true },
-});
+const paymentSchema = mongoose.Schema(
+	{
+		name: { type: String, require: true, trim: true },
+		date: { type: String, require: true, trim: true },
+		amount: { type: Number, require: true, trim: true },
+	},
+	{ timestamps: true }
+);
 
 const Payment = mongoose.model("payments", paymentSchema);
 
