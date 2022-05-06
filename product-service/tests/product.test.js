41cf9363e0bca40903376d57cc7ccfb4d17b4872
Thushara Thiwanka <46595464+thusharathiwanka@users.noreply.github.com>
2022-05-06 12:14:28 +0530

    UPDATE: product tests updates

--- product-service/tests/product.test.js
+++ product-service/tests/product.test.js
@@ -8,7 +8,7 @@ describe("POST @ products/ endpoint", () => {
 				description: "Pure white chocolate",
 				price: 250.0,
 				status: "active",
-				src: "https://freshmartctg.com/wp-content/uploads/2021/08/Amul-Dark-Chocolate-150-gm-min.png",
+				src: "https://www.thechocolatehouse.lk/1698-thickbox_default/tch869-toren-white-chooclate-100g.jpg",
 			});
 
 			expect(res.status).toEqual(201);
@@ -21,7 +21,7 @@ describe("POST @ products/ endpoint", () => {
 describe("GET @ products/ endpoint", () => {
 	it("should return an products array and return status code 200", async () => {
 		try {
-			const res = axios.get("http://localhost:5000/customers");
+			const res = axios.get("http://localhost:5000/products");
 
 			expect(res.status).toEqual(200);
 			expect(typeof res.data).toEqual("Object");
